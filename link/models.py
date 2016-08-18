a18a1e4b8247ab312db268f781d2944a5be57d0b
qoda <jpbydendyk@gmail.com>
2016-08-18 11:58:50 +0200

    Added link model

--- link/models.py
+++ link/models.py
@@ -1,1 +1,75 @@
+from django.core.urlresolvers import reverse, NoReverseMatch
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
 
+
+class Link(models.Model):
+    title = models.CharField(
+        max_length=256,
+        help_text="A short descriptive title.",
+    )
+    subtitle = models.CharField(
+        max_length=256,
+        blank=True,
+        null=True,
+        help_text="Some titles may be the same. A subtitle makes a distinction. It is not displayed on the site.",
+    )
+    view_name = models.CharField(
+        max_length=256,
+        help_text="View name to which this link will redirect.",
+        blank=True,
+        null=True,
+    )
+    target_content_type = models.ForeignKey(
+        ContentType, blank=True, null=True, related_name="link_target_content_type"
+    )
+    target_object_id = models.PositiveIntegerField(blank=True, null=True)
+    target = generic.GenericForeignKey(
+        "target_content_type", "target_object_id"
+    )
+    url = models.CharField(
+        max_length=256,
+        help_text="URL to which this link will redirect.",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        ordering = ("title", "subtitle")
+
+    def __unicode__(self):
+        if self.subtitle:
+            return "%s (%s)" % (self.title, self.subtitle)
+        else:
+            return self.title
+
+    def get_absolute_url(self):
+        """
+        Returns URL to which link should redirect based on a reversed view
+        name, category, target or explicitly provided URL.
+        """
+        if self.view_name:
+            return reverse(self.view_name)
+        elif self.target:
+            return self.target.get_absolute_url()
+        else:
+            # Django can be served in a subdirectory. Transparently fix urls.
+            if "://" in self.url:
+                return self.url
+
+            # Urls not starting with a slash probably do so with reason. Skip.
+            if not self.url.startswith("/"):
+                return self.url
+
+            # Request is not available here so use reverse to determine root
+            try:
+                root = reverse("home")
+            except NoReverseMatch:
+                return self.url
+
+            # /abc and /today/abc must be transformed into /today/abc
+            if not self.url.startswith(root):
+                return root.rstrip("/") + "/" + self.url.lstrip("/")
+
+            return self.url
