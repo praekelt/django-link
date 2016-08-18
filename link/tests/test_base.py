35100ec4bc540e5658c78a834d27188d53f2488a
qoda <jpbydendyk@gmail.com>
2016-08-18 12:20:21 +0200

    Stubbed util tests

--- link/tests/test_base.py
+++ link/tests/test_base.py
@@ -2,3 +2,10 @@
 
 from django.test import TestCase
 from django.test.client import Client, RequestFactory
+
+from link import utils, models
+
+
+class UtilsTestCase(TestCase):
+    def test_get_view_names(self):
+        print utils.get_view_names()
