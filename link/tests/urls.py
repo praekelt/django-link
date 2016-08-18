d33d2bd652164fc7ed30cda34ee5b21f3097203b
qoda <jpbydendyk@gmail.com>
2016-08-18 13:41:37 +0200

    Removed 'old-style' urls

--- link/tests/urls.py
+++ link/tests/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from link.tests import views
 
 
-urlpatterns = patterns(
-    "",
+urlpatterns = [
     url(
         r"^link/1/$", views.TestView.as_view(), name="link-1"
     ),
     url(
         r"^link/2/$", views.TestView.as_view(), name="link-2"
     ),
-)
+]
