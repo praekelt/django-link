44394b13fcdde11ddfb6f47e2359524deddaa5e1
qoda <jpbydendyk@gmail.com>
2016-08-18 13:40:48 +0200

    Added model tests

--- link/tests/test_base.py
+++ link/tests/test_base.py
@@ -9,3 +9,17 @@ from link import utils, models
 class UtilsTestCase(TestCase):
     def test_get_view_names(self):
         self.assertEqual(utils.get_view_names(), ["link-1", "link-2"])
+
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.link_data = {
+            "title": "Link 1 Title",
+            "subtitle": "Link 1 Subtitle",
+            "url": "/link-1/"
+        }
+
+    def test_link(self):
+        link = models.Link.objects.create(**self.link_data)
+        for key, value in self.link_data.items():
+            self.assertEqual(getattr(link, key), value)
