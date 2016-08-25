d137cee16cc5eb74fcec206a6966b9e956b361df
qoda <jpbydendyk@gmail.com>
2016-08-25 16:30:56 +0200

    Fixed view name discovery to include namespace

--- link/utils.py
+++ link/utils.py
@@ -3,17 +3,24 @@ from importlib import import_module
 from django.conf import settings
 from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
 
+from link import SETTINGS
+
 
 URLS = import_module(settings.ROOT_URLCONF).urlpatterns
 
 
 def get_view_names(urlpatterns=URLS, view_names=[], namespace=None):
-    for pattern in urlpatterns:
-        if isinstance(pattern, RegexURLResolver):
-            get_view_names(pattern.url_patterns, view_names, pattern.namespace)
-        elif isinstance(pattern, RegexURLPattern):
-            view_name = pattern.name
-            view_names.append(view_name)
+    if namespace not in SETTINGS.get("EXCLUDED_VIEWNAME_CHOICES", []):
+        for pattern in urlpatterns:
+            if isinstance(pattern, RegexURLResolver):
+                get_view_names(
+                    pattern.url_patterns, view_names, pattern.namespace
+                )
+            elif isinstance(pattern, RegexURLPattern):
+                view_name = pattern.name
+                if namespace:
+                    view_name = "%s:%s" % (namespace, pattern.name)
+                view_names.append(view_name)
     return view_names
 
 
