cf37886c01e59b643a591545fb4b3e8b03b3f87b
qoda <jpbydendyk@gmail.com>
2016-08-26 09:54:49 +0200

    Fixed test settings

--- link/tests/settings/19.py
+++ link/tests/settings/19.py
@@ -9,50 +9,44 @@ DATABASES = {
 
 ROOT_URLCONF = "link.tests.urls"
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
+    "test_without_migrations",
     "link",
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.admin",
-)
+    "django.contrib.staticfiles"
+]
 
-MIDDLEWARE_CLASSES = (
-    "django.middleware.common.CommonMiddleware",
+MIDDLEWARE_CLASSES = [
+    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware"
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.template.context_processors.debug",
-    "django.template.context_processors.i18n",
-    "django.template.context_processors.media",
-    "django.template.context_processors.static",
-    "django.template.context_processors.tz",
-    "django.template.context_processors.request",
-    "django.contrib.messages.context_processors.messages"
-)
+    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware"
+]
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
 SITE_ID = 1
-
+STATIC_URL = "/static/"
 SECRET_KEY = "SECRET_KEY"
