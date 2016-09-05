1093b71409fa4b6b9b2adf5077ded8ce5d8522b4
qoda <jpbydendyk@gmail.com>
2016-09-05 13:23:02 +0200

    Added view param test case

--- link/tests/test_base.py
+++ link/tests/test_base.py
@@ -19,6 +19,10 @@ class ModelTestCase(TestCase):
             "title": "Link 1 Title",
             "url": "/link-1/"
         }
+        self.viewparam_data = {
+            "key": "slug",
+            "value": "1",
+        }
 
     def test_link(self):
         link = models.Link.objects.create(**self.link_data)
@@ -41,6 +45,15 @@ class ModelTestCase(TestCase):
         link.view_name = None
         link.target = content_link
         self.assertEqual(link.absolute_url, "/content/1/")
+
+    def test_viewparam(self):
+        link = models.Link.objects.create(**self.link_data)
+        viewparam = models.ViewParam.objects.create(**self.viewparam_data)
+        link.view_params.add(viewparam)
+        link.view_name = "link:link-detail"
+
+        # ensure the url is returned correctly
+        self.assertEqual(link.absolute_url, "/link/1/")
 
 
 class AdminTestCase(TestCase):
